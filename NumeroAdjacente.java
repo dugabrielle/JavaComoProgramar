0a7ac788968ff19fd3720ef9b804a046b9a62c67
dugabrielle <gabduarte113@gmail.com>
2023-05-10 15:52:19 -0300

    numero adjacente

---
+++ NumeroAdjacente.java
@@ -0,0 +1,15 @@
+public class NumeroAdjacente {
+
+    public static void main (String[] args){
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+
+        System.out.println(a + " " + b + " " + c + " " + d + "\n");
+        System.out.printf("%d %d %d %d",
+                a, b, c, d);
+
+    }
+}
